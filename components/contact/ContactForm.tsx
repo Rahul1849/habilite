44affbe8388e14f601c2e6ca88c87d6cc9980d59
= <=>
2025-11-15 16:56:43 +0530

    code updated

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -36,7 +36,7 @@ export default function ContactForm() {
         </div>
         <h3 className="text-2xl font-bold text-gray-900 mb-2">Message Sent!</h3>
         <p className="text-gray-600">We&apos;ll get back to you soon.</p>
-      </motion.div>
+      </div>
     )
   }
 
