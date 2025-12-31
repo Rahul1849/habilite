6ef6ae5d709ab911c7a694dd3b39e6dba279eca3
= <=>
2025-12-31 15:33:11 +0530

    updated meta tags

--- app/laparoscopic-surgery/gallbladder-surgery/page.tsx
+++ app/laparoscopic-surgery/gallbladder-surgery/page.tsx
@@ -124,6 +124,17 @@ export const metadata: Metadata = {
       },
     ],
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
   alternates: {
     canonical:
       "https://www.habiliteclinics.com/best-gallbladder-stone-surgeon-delhi",
